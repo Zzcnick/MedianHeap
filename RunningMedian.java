dd2817caf80cd8d7e3e60b0672fef21f529ff410
Joel Ye <jye5@stuy.edu>
2016-05-26 21:18:21 -0400

    Add basis

---
+++ RunningMedian.java
@@ -0,0 +1,13 @@
+public class RunningMedian{
+		ALHeap minHeap;
+		ALHeap maxHeap;
+		int median;
+	
+		public class RunningMedian(){
+			minHeap = new ALHeap();
+			maxHeap = new ALHeap();
+			
+		}
+
+
+}
